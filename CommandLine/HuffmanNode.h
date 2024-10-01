ccb17350639a8df6927852f7a25ddb73f0a87b4d
Houston Brown <80162431+teagist@users.noreply.github.com>
2024-10-01 15:38:08 -0500

    Create HuffmanNode.h

---
+++ CommandLine/HuffmanNode.h
@@ -0,0 +1,41 @@
+//****************************************************************************
+//	Header File for Huffman Tree Node Class
+//	Programmed by: Houston Brown
+//	Last Compiled Date: 10/31/2023
+//****************************************************************************
+
+#ifndef _HuffmanNode_H
+#define _HuffmanNode_H
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <string>
+#include <cmath>
+#include "Logger.h"
+using namespace std;
+
+
+class HuffmanTree;
+class BinaryHeap;
+
+
+
+class HNode
+{
+	char item;
+    int freq;
+    HNode* left;
+    HNode* right;
+
+    HNode(char newItem, int newFreq): item(newItem), freq(newFreq), 
+								      left(NULL), right(NULL) {};
+								      
+    HNode(char newItem, int newFreq, HNode* newLeft, HNode* newRight): 
+			  item(newItem), freq(newFreq), left(newLeft), right(newRight) {};
+	
+    friend class HuffmanTree;
+    friend class BinaryHeap;
+};
+
+#endif
